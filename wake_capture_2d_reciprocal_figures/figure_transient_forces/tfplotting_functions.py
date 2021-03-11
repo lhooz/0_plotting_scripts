a86f49cd8bbd0607c07300d83673ff91603e0e54
hao <hao.lee0019@gmail.com>
2021-03-11 12:30:40 +0000

    added reciprocal transient figures

---
+++ wake_capture_2d_reciprocal_figures/figure_transient_forces/tfplotting_functions.py
@@ -0,0 +1,224 @@
+"""fuctions for plotting cfd run results against ref data"""
+
+import csv
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import UnivariateSpline
+
+
+def read_cfd_data(cfd_data_file):
+    """read cfd results force coefficients data"""
+    cf_array = []
+    with open(cfd_data_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='\t')
+        line_count = 0
+
+        for row in csv_reader:
+            if line_count <= 14:
+                line_count += 1
+            else:
+                cf_array.append([
+                    float(row[0]),
+                    float(row[1]),
+                    float(row[2]),
+                    float(row[3]),
+                    float(row[4]),
+                    float(row[5]),
+                    float(row[6])
+                ])
+                line_count += 1
+
+        print(f'Processed {line_count} lines in {cfd_data_file}')
+
+    cf_array = np.array(cf_array)
+    return cf_array
+
+
+def cf_plotter(data_array, marks, time_to_plot, show_range, image_out_path,
+               cycle_time, plot_mode):
+    """
+    function to plot cfd force coefficients results
+    """
+    plt.rcParams.update({
+        # "text.usetex": True,
+        'mathtext.fontset': 'stix',
+        'font.family': 'STIXGeneral',
+        'font.size': 18,
+        'figure.figsize': (14, 8),
+        'lines.linewidth': 2.0,
+        'lines.markersize': 0.1,
+        'lines.markerfacecolor': 'white',
+        'figure.dpi': 100,
+        'figure.subplot.left': 0.25,
+        'figure.subplot.right': 0.75,
+        'figure.subplot.top': 0.75,
+        'figure.subplot.bottom': 0.2,
+        'figure.subplot.wspace': 0.1,
+        'figure.subplot.hspace': 0.1,
+    })
+    legendx = 0.5
+    legendy = 1.10
+
+    cf_array = np.array(data_array)
+    markre = marks[0]
+    markpa = marks[1]
+    legends = marks[2]
+    range_cl = show_range[0]
+    range_cd = show_range[1]
+
+    fig, axs_1 = plt.subplots(1, 1)
+    axs = np.array([[axs_1]])
+    if plot_mode == 'against_t':
+        for rei in range(len(markre)):
+            ax = axs[:, rei]
+            datano = rei * len(markpa) * len(legends)
+            for i in range(len(markpa) * len(legends)):
+                ax_mk = int(i / 4)
+                legendi = np.mod(i, 4)
+                ax[ax_mk].plot(cf_array[datano + i][:, 0] / cycle_time,
+                               cf_array[datano + i][:, 3],
+                               label=legends[legendi])
+
+                if time_to_plot != 'all':
+                    ax[ax_mk].set_xlim(time_to_plot)
+                if range_cl != 'all':
+                    ax[ax_mk].set_ylim(range_cl)
+
+            for ai in range(len(ax)):
+                if ai == 0:
+                    marky_loc = ax[0].get_ylim()[1] + 0.27 * (
+                        ax[0].get_ylim()[1] - ax[0].get_ylim()[0])
+                    ax[0].annotate(s=markre[rei],
+                                   xy=(1.1, marky_loc),
+                                   ha='center',
+                                   va='center',
+                                   annotation_clip=False)
+
+                ax[ai].axhline(y=0, color='k', linestyle='-.', linewidth=0.5)
+                ax[ai].axvline(x=1, color='k', linestyle='-', linewidth=0.5)
+
+                if ax[0] == axs[:, 0][0]:
+                    ax[ai].set_ylabel(r'$C_L$')
+                else:
+                    ax[ai].set_yticklabels([])
+
+                if ai < len(ax) - 1:
+                    ax[ai].set_xticklabels([])
+
+                if ax[0] == axs[:, -1][0]:
+                    markx_loc = ax[ai].get_xlim()[1] + 0.15 * (
+                        ax[ai].get_xlim()[1] - ax[ai].get_xlim()[0])
+                    marky_loc = ax[ai].get_ylim()[0] + 0.5 * (
+                        ax[ai].get_ylim()[1] - ax[ai].get_ylim()[0])
+
+                    ax[ai].annotate(s=markpa[ai],
+                                    xy=(markx_loc, marky_loc),
+                                    ha='center',
+                                    va='center',
+                                    annotation_clip=False)
+
+                    ax[0].legend(loc='upper center',
+                                 bbox_to_anchor=(legendx, legendy),
+                                 ncol=4,
+                                 fontsize='small',
+                                 frameon=False)
+
+            texty_loc = ax[-1].get_ylim()[0] + 0.05 * (ax[-1].get_ylim()[1] -
+                                                       ax[-1].get_ylim()[0])
+            ax[-1].annotate(s='1st stroke',
+                            xy=(0.55, texty_loc),
+                            ha='center',
+                            va='center',
+                            annotation_clip=False)
+            ax[-1].annotate(s='reverse stroke',
+                            xy=(1.55, texty_loc),
+                            ha='center',
+                            va='center',
+                            annotation_clip=False)
+
+            ax[-1].set_xlabel(r'$\^t$')
+
+    title = 'transient cl'
+    out_image_file = os.path.join(image_out_path, title + '.png')
+    fig.savefig(out_image_file)
+
+    fig2, axs_2 = plt.subplots(1, 1)
+    axs2 = np.array([[axs_2]])
+    if plot_mode == 'against_t':
+        for rei in range(len(markre)):
+            ax2 = axs2[:, rei]
+            datano = rei * len(markpa) * len(legends)
+            for i in range(len(markpa) * len(legends)):
+                ax2_mk = int(i / 4)
+                legendi = np.mod(i, 4)
+                ax2[ax2_mk].plot(cf_array[datano + i][:, 0] / cycle_time,
+                                 cf_array[datano + i][:, 1],
+                                 label=legends[legendi])
+
+                if time_to_plot != 'all':
+                    ax2[ax2_mk].set_xlim(time_to_plot)
+                if range_cd != 'all':
+                    ax2[ax2_mk].set_ylim(range_cd)
+
+            for ai in range(len(ax2)):
+                if ai == 0:
+                    marky_loc = ax2[0].get_ylim()[1] + 0.27 * (
+                        ax2[0].get_ylim()[1] - ax2[0].get_ylim()[0])
+                    ax2[0].annotate(s=markre[rei],
+                                    xy=(1.1, marky_loc),
+                                    ha='center',
+                                    va='center',
+                                    annotation_clip=False)
+
+                ax2[ai].axhline(y=0, color='k', linestyle='-.', linewidth=0.5)
+                ax2[ai].axvline(x=1, color='k', linestyle='-', linewidth=0.5)
+
+                if ax2[0] == axs2[:, 0][0]:
+                    ax2[ai].set_ylabel(r'$C_D$')
+                else:
+                    ax2[ai].set_yticklabels([])
+
+                if ai < len(ax2) - 1:
+                    ax2[ai].set_xticklabels([])
+
+                if ax2[0] == axs2[:, -1][0]:
+                    markx_loc = ax2[ai].get_xlim()[1] + 0.15 * (
+                        ax2[ai].get_xlim()[1] - ax2[ai].get_xlim()[0])
+                    marky_loc = ax2[ai].get_ylim()[0] + 0.5 * (
+                        ax2[ai].get_ylim()[1] - ax2[ai].get_ylim()[0])
+
+                    ax2[ai].annotate(s=markpa[ai],
+                                     xy=(markx_loc, marky_loc),
+                                     ha='center',
+                                     va='center',
+                                     annotation_clip=False)
+
+                    ax2[0].legend(loc='upper center',
+                                  bbox_to_anchor=(legendx, legendy),
+                                  ncol=4,
+                                  fontsize='small',
+                                  frameon=False)
+
+            texty_loc = ax2[-1].get_ylim()[0] + 0.05 * (ax2[-1].get_ylim()[1] -
+                                                        ax2[-1].get_ylim()[0])
+            ax2[-1].annotate(s='stroke motion',
+                             xy=(0.55, texty_loc),
+                             ha='center',
+                             va='center',
+                             annotation_clip=False)
+            ax2[-1].annotate(s='wake effect',
+                             xy=(1.55, texty_loc),
+                             ha='center',
+                             va='center',
+                             annotation_clip=False)
+
+            ax2[-1].set_xlabel(r'$\^t$')
+
+    title2 = 'transient cd'
+    out_image_file = os.path.join(image_out_path, title2 + '.png')
+    fig2.savefig(out_image_file)
+    plt.show()
+
+    return fig, fig2
---
+++ wake_capture_2d_reciprocal_figures/figure_transient_forces/transientf_figure.py
@@ -0,0 +1,54 @@
+"""plotting cfd run results"""
+import os
+
+from tfplotting_functions import cf_plotter, read_cfd_data
+
+#-------------input plot control----------
+pa = [90]
+Re = [1400]
+stroke = [3, 5, 7]
+at = [0.25]
+pt = [0.25]
+#-----------------------------------------
+cfd_data_list = []
+for re in Re:
+    for pai in pa:
+        for s in stroke:
+            for a in at:
+                for p in pt:
+                    cfd_data_name = 'Re' + '{0:.1f}'.format(
+                        re) + '_stroke' + '{0:.1f}'.format(
+                            s) + '_acf' + '{0:.3g}'.format(
+                                a) + '_pf' + '{0:.3g}'.format(
+                                    p) + '_pa' + '{0:.1f}'.format(pai)
+                    cfd_data_list.append(cfd_data_name)
+#-----------------------------------------
+markre = [r'$Re = ' + '{0:.0f}'.format(x) + '$' for x in Re]
+marksc = [r'$s/c = ' + '{0:.1f}'.format(x) + '$' for x in stroke]
+markpa = [r'$\theta = ' + '{0:.0f}'.format(x) + '$' for x in pa]
+marks = [markre, markpa, marksc]
+#---------------------------------------
+time_to_plot = 'all'
+coeffs_show_range = 'all'
+time_to_plot = [0, 2.0]
+show_range_cl = [-2.0, 10.0]
+show_range_cd = [-11.5, 13]
+cycle_time = 1.0
+#---------------------------------------
+show_range = [show_range_cl, show_range_cd]
+cd = os.getcwd()
+wd = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(cd))),
+                  'wake_capture_2d_reciprocal_data/symmetric')
+image_out_path = cd
+#---------------------------------------
+cf_array = []
+for cfi in cfd_data_list:
+    cfd_datai = os.path.join(wd, 'SIM_RESULTS', cfi)
+    cf_arrayi = read_cfd_data(cfd_datai)
+    cf_array.append(cf_arrayi)
+
+data_array = cf_array
+#---------------------------------------
+
+cf_plotter(data_array, marks, time_to_plot, show_range, image_out_path,
+           cycle_time, 'against_t')
