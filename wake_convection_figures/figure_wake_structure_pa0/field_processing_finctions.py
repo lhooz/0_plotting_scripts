c41357954998539ba680bdf41a7c25adb393f7f7
hao <hao.lee0019@gmail.com>
2021-03-25 23:51:50 +0000

    added pa0 wake flow

---
+++ wake_convection_figures/figure_wake_structure_pa0/field_processing_finctions.py
@@ -0,0 +1,287 @@
+"""circulation processing functions"""
+
+import csv
+import os
+import shutil
+
+import matplotlib.patches as patches
+import matplotlib.path as path
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.interpolate
+from matplotlib import colors
+from scipy.ndimage import zoom
+from scipy.ndimage.filters import gaussian_filter
+
+
+def read_vorfield(field_data_file, Uref):
+    """read field (vorticity or q) data"""
+    vor_array = []
+    with open(field_data_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                vor_array.append(
+                    [-float(row[0]),
+                     float(row[1]),
+                     float(row[3]) / Uref])
+                line_count += 1
+
+        print(f'Processed {line_count} lines in {field_data_file}')
+
+    vor_array = np.array(vor_array)
+    return vor_array
+
+
+def read_qfield(field_data_file, Uref):
+    """read field (vorticity or q) data"""
+    refC = Uref * Uref
+    q_array = []
+    with open(field_data_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                q_array.append(
+                    [-float(row[0]),
+                     float(row[1]),
+                     float(row[3]) / refC])
+                line_count += 1
+
+        print(f'Processed {line_count} lines in {field_data_file}')
+
+    q_array = np.array(q_array)
+    return q_array
+
+
+def read_vfield(field_data_file, Uref):
+    """read field (vorticity or q) data"""
+    v_array = []
+    with open(field_data_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                v_array.append([
+                    -float(row[0]),
+                    float(row[1]), -float(row[3]) / Uref,
+                    float(row[4]) / Uref
+                ])
+                line_count += 1
+
+        print(f'Processed {line_count} lines in {field_data_file}')
+
+    v_array = np.array(v_array)
+    return v_array
+
+
+def read_wgeo(wgeo_data_file):
+    """read wing geometry data"""
+    wgeo_array = []
+    with open(wgeo_data_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                wgeo_array.append([-float(row[0]), float(row[1])])
+                line_count += 1
+
+        print(f'Processed {line_count} lines in {wgeo_data_file}')
+
+    wgeo_array = np.array(wgeo_array)
+
+    # ----- sorting points in clockwise order ---
+    x = wgeo_array[:, 0]
+    y = wgeo_array[:, 1]
+    cx = np.mean(x)
+    cy = np.mean(y)
+    a = np.arctan2(y - cy, x - cx)
+    order = a.ravel().argsort()
+    x = x[order]
+    y = y[order]
+    wgeo_array = np.vstack((x, y))
+
+    wgeo_array = np.transpose(wgeo_array)
+    w_centroid = np.array([cx, cy])
+
+    return wgeo_array, w_centroid
+
+
+def grid_vorz(window, resolution, vor_array):
+    """grid interpolation for vorticity data"""
+    grid_x, grid_y = np.mgrid[window[0]:window[1]:resolution[0] * 1j,
+                              window[2]:window[3]:resolution[1] * 1j]
+
+    grid_vz = scipy.interpolate.griddata(vor_array[:, 0:2],
+                                         vor_array[:, 2], (grid_x, grid_y),
+                                         method='nearest')
+
+    return grid_x, grid_y, grid_vz
+
+
+def grid_ufield(window, resolution, ufield_array):
+    """grid interpolation for vorticity data"""
+    grid_x, grid_y = np.mgrid[window[0]:window[1]:resolution[0] * 1j,
+                              window[2]:window[3]:resolution[1] * 1j]
+
+    grid_ux = scipy.interpolate.griddata(ufield_array[:, 0:2],
+                                         ufield_array[:, 2], (grid_x, grid_y),
+                                         method='nearest')
+
+    grid_uy = scipy.interpolate.griddata(ufield_array[:, 0:2],
+                                         ufield_array[:, 3], (grid_x, grid_y),
+                                         method='nearest')
+
+    return grid_x, grid_y, grid_ux, grid_uy
+
+
+def single_plot_field(images, axto_plot, window, grid_x, grid_y, sImgdata,
+                      sCtrdata, vdata, wdata, imnorm, levels):
+    """plot one single field data"""
+    images.append(
+        axto_plot.imshow(sImgdata,
+                         cmap='RdBu',
+                         norm=imnorm,
+                         aspect='equal',
+                         extent=window,
+                         origin='lower',
+                         interpolation='bicubic'))
+    axto_plot.contour(sCtrdata,
+                      levels,
+                      linewidths=0.8,
+                      # linestyles='--',
+                      colors='k',
+                      extent=window,
+                      origin='lower')
+    axto_plot.quiver(grid_x,
+                     grid_y,
+                     vdata[0],
+                     vdata[1],
+                     units='height',
+                     scale=50,
+                     width=0.0025,
+                     headwidth=3.5)
+
+    nverts = len(wdata)
+    codes = np.ones(nverts, int) * path.Path.LINETO
+    codes[0] = path.Path.MOVETO
+    codes[-1] = path.Path.CLOSEPOLY
+    wgeopatch = path.Path(wdata, codes)
+    patch = patches.PathPatch(wgeopatch,
+                              linewidth=0.2,
+                              facecolor='k',
+                              edgecolor='k',
+                              alpha=1.0)
+    axto_plot.add_patch(patch)
+
+    print('plotted image no = %s\n' % '{0:.0f}'.format(len(images)))
+
+    return axto_plot
+
+
+def field_plot(windows, field_data, wgeo_data, marks, oimage_file, mode):
+    """plot field data"""
+    plt.rcParams.update({
+        # "text.usetex": True,
+        'mathtext.fontset': 'stix',
+        'font.family': 'STIXGeneral',
+        'font.size': 18,
+        'figure.figsize': (12, 12),
+        'lines.linewidth': 0.5,
+        'lines.markersize': 0.1,
+        'lines.markerfacecolor': 'white',
+        'figure.dpi': 300,
+    })
+    marksc = marks[0]
+    markre = marks[1]
+    no_r = len(markre)
+    no_c = len(marksc)
+    imnorm = colors.Normalize(vmin=-50, vmax=50)
+    levels = np.linspace(0.6, 100, 4)
+    zoom_order = 5
+
+    images = []
+    ax_all = []
+    #----plot different pa data----
+    gs_kw = dict(left=0.125,
+                 right=0.85,
+                 top=0.85,
+                 bottom=0.15,
+                 wspace=0.05,
+                 hspace=0.05,
+                 width_ratios=[4, 5.5, 8.5])
+
+    fig, ax = plt.subplots(nrows=no_r, ncols=no_c, gridspec_kw=gs_kw)
+    for ci in range(no_c):
+        axci = ax[:, ci]
+        for ri in range(no_r):
+            axre = axci[ri]
+            grid_x = field_data[ci][ri][0]
+            grid_y = field_data[ci][ri][1]
+            sImgdatai = field_data[ci][ri][2]
+            sCtrdatai = field_data[ci][ri][3]
+            vdatai = field_data[ci][ri][4]
+
+            wdatai = wgeo_data[ci][ri]
+
+            sImgdatai = zoom(sImgdatai, zoom_order)
+            sImgdatai = gaussian_filter(sImgdatai, sigma=3.0)
+            sCtrdatai = zoom(sCtrdatai, zoom_order)
+            sCtrdatai = gaussian_filter(sCtrdatai, sigma=3.0)
+            single_plot_field(images, axre, windows[ci], grid_x, grid_y,
+                              sImgdatai, sCtrdatai, vdatai, wdatai, imnorm,
+                              levels)
+
+            axre.set_xticklabels([])
+            axre.set_yticklabels([])
+
+            if ri == 0:
+                markx_loc = axre.get_xlim()[0] + 0.5 * (axre.get_xlim()[1] -
+                                                        axre.get_xlim()[0])
+                marky_loc = axre.get_ylim()[1] + 0.1 * (axre.get_ylim()[1] -
+                                                        axre.get_ylim()[0])
+                axre.annotate(s=marksc[ci],
+                              xy=(markx_loc, marky_loc),
+                              ha='center',
+                              va='center',
+                              annotation_clip=False)
+
+            if ci == no_c - 1:
+                markx_loc = axre.get_xlim()[1] + 0.2 * (axre.get_xlim()[1] -
+                                                        axre.get_xlim()[0])
+                marky_loc = axre.get_ylim()[0] + 0.5 * (axre.get_ylim()[1] -
+                                                        axre.get_ylim()[0])
+                axre.annotate(s=markre[ri],
+                              xy=(markx_loc, marky_loc),
+                              ha='center',
+                              va='center',
+                              annotation_clip=False)
+
+            ax_all.append(axre)
+
+    cb = fig.colorbar(images[-1],
+                      ax=ax_all,
+                      orientation='horizontal',
+                      fraction=0.1,
+                      shrink=0.2,
+                      pad=0.03)
+    cb.ax.set_xlabel(r'$\omega$*')
+    cb.ax.xaxis.set_label_coords(-0.12, 1.5)
+
+    if mode == 'save':
+        plt.savefig(oimage_file)
+    elif mode == 'show':
+        plt.show()
---
+++ wake_convection_figures/figure_wake_structure_pa0/flow_field_figure.py
@@ -0,0 +1,117 @@
+"""main script for circulation processing"""
+
+import os
+import shutil
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from field_processing_finctions import (field_plot, grid_vorz, grid_ufield,
+                                        read_vorfield, read_qfield,
+                                        read_vfield, read_wgeo)
+
+#--------------figure parameters------------
+window_sc15 = [-1.0, 3.0, -2.5, 2.5]
+window_sc30 = [-1.0, 4.5, -2.5, 2.5]
+window_sc60 = [-1.0, 7.5, -2.5, 2.5]
+resolution = [200, 200]  #--interpolate res for vorticity field--
+#--interpolate res for velocity field--
+res_vector_sc15 = [30, 40]
+res_vector_sc30 = [40, 40]
+res_vector_sc60 = [60, 40]
+resolution_vector = [res_vector_sc15, res_vector_sc30, res_vector_sc60]
+#--------------------------------------
+Uref = [1.786, 3.571, 7.143]  #--ref velocity--
+data_time_increment = 0.1
+#--------data parameters------------
+Re = [100.0, 1000.0, 10000.0]
+stroke = [1.5, 3.0, 6.0]
+pa = [0.0]
+#-----------------------------------
+windows = [window_sc15, window_sc30, window_sc60]
+marksc = [r'$s/c = ' + '{0:.1f}'.format(x) + '$' for x in stroke]
+markpa = [r'$Re = ' + '{0:.0f}'.format(x) + '$' for x in Re]
+marks = [marksc, markpa]
+#-----------------------------------
+cwd = os.getcwd()
+data_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(cwd))),
+                        'wake_convection_data/4_FIELD_RESULTS')
+oimage_file = os.path.join(cwd, 'wake_structure_pa0.png')
+#-----------------------------------
+fdata_all = []
+wdata_all = []
+for si, window, Urefi, res_vectori in zip(stroke, windows, Uref,
+                                          resolution_vector):
+    #------read data for each Re-------------
+    fdata_si = []
+    wdata_si = []
+    for rei in Re:
+        pf = 0.25
+
+        case_name_t10 = 'Re' + '{0:.1f}'.format(
+            rei) + '_stroke' + '{0:.1f}'.format(
+                si) + '_acf0.25_pf' + '{0:.3g}'.format(
+                    pf) + '_pa' + '{0:.1f}'.format(pa[0])
+
+        case_dir = os.path.join(data_dir, case_name_t10)
+        vorz_folder = os.path.join(case_dir, 'vorz_data')
+        q_folder = os.path.join(case_dir, 'q_data')
+        ufield_folder = os.path.join(case_dir, 'ufield_data')
+        wgeo_folder = os.path.join(case_dir, 'geop_data')
+
+        #----------------------------------------
+        time_series_names = [
+            f.name for f in os.scandir(vorz_folder) if f.is_file()
+        ]
+        time_series = [x.split('_')[-1] for x in time_series_names]
+        time_series = [int(x.split('.')[0]) for x in time_series]
+        start_t = np.min(np.array(time_series))
+        end_t = np.max(np.array(time_series))
+
+        for ti in range(start_t, end_t + 1):
+            timei = (ti + 1) * data_time_increment
+            if 0.99 <= timei <= 1.01:
+                time_instance = str(ti).zfill(4)
+                #--------setting up file dirs-----------
+                vorz_data_file = os.path.join(vorz_folder,
+                                              'vorz_' + time_instance + '.csv')
+                q_data_file = os.path.join(q_folder,
+                                           'q_' + time_instance + '.csv')
+                ufield_data_file = os.path.join(
+                    ufield_folder, 'ufield_' + time_instance + '.csv')
+                wgeo_data_file = os.path.join(wgeo_folder,
+                                              'geop_' + time_instance + '.csv')
+
+                vorz_data = read_vorfield(vorz_data_file, Urefi)
+                q_data = read_qfield(q_data_file, Urefi)
+                ufield_data = read_vfield(ufield_data_file, Urefi)
+                wgeo_data, w_centroid = read_wgeo(wgeo_data_file)
+                #-------------------------------------
+                grid_x, grid_y, grid_vz = grid_vorz(window, resolution,
+                                                    vorz_data)
+                grid_x, grid_y, grid_q = grid_vorz(window, resolution, q_data)
+                grid_x, grid_y, grid_ux, grid_uy = grid_ufield(
+                    window, res_vectori, ufield_data)
+                #-----write wing centroid history-----
+                centroid_ti = [
+                    str(timei),
+                    str(w_centroid[0]),
+                    str(w_centroid[1])
+                ]
+
+                ufield_gridx = grid_x.T
+                ufield_gridy = grid_y.T
+                sImgdata = grid_vz.T
+                sCtrdata = grid_q.T
+                vdata = [grid_ux.T, grid_uy.T]
+
+                fdata_si.append(
+                    [ufield_gridx, ufield_gridy, sImgdata, sCtrdata, vdata])
+                wdata_si.append(wgeo_data)
+        #----------------------------------------
+    fdata_all.append(fdata_si)
+    wdata_all.append(wdata_si)
+# -------------plot all vortices-----------
+field_plot(windows, fdata_all, wdata_all, marks, oimage_file, 'save')
+plt.close()
+# ----------------------------------------------
