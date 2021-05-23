e053e791f0cc0d920b814c9f38b24bd5d1052f8a
hao <hao.lee0019@gmail.com>
2021-05-23 19:22:09 +0100

    added wc appendix figures

---
+++ wake_convection_appendix/figure_mean_forces/meanf_figure.py
@@ -0,0 +1,64 @@
+"""plotting cfd run results"""
+import os
+
+from mfplotting_functions import cf_plotter90, read_cfd_data
+
+#-------------input plot control----------
+Re = [100, 1000]
+stroke = [3, 5, 7, 9]
+at = [0.125, 0.25, 0.5]
+pt90 = [0.125, 0.25, 0.5]
+pt0 = [0.25]
+#-----------------------------------------
+cfd_data_list_pa90 = []
+for p in pt90:
+    for a in at:
+        for re in Re:
+            for s in stroke:
+                pafolder_pa90 = 'SIM_RESULTS_pa90'
+                cfd_data_name = 'Re' + '{0:.1f}'.format(
+                    re) + '_stroke' + '{0:.1f}'.format(
+                        s) + '_acf' + '{0:.3g}'.format(
+                            a) + '_pf' + '{0:.3g}'.format(p)
+                cfd_data_path_pa90 = os.path.join(pafolder_pa90, cfd_data_name)
+                cfd_data_list_pa90.append(cfd_data_path_pa90)
+#-----------------------------------------
+x_data = stroke
+markr90 = [r'$\^p_t = ' + '{0:.3g}'.format(x) + '$' for x in pt90]
+markc = [r'$\^a_t = ' + '{0:.3g}'.format(x) + '$' for x in at]
+marks90 = [markr90, markc]
+legends = [r'$Re = 10^2$', r'$Re = 10^3$']
+#---------------------------------------
+# x_range = 'all'
+# y_range = 'all'
+x_range = [2, 10]
+cl_range = [-0.5, 0.5]
+rl_range = [-0.5, 0.5]
+cd_range = [-0.5, 0.5]
+rd_range = [-0.5, 0.5]
+rld_range = [-15, 15]
+y_range = [cl_range, rl_range, cd_range, rd_range, rld_range]
+y_label = [
+    r'$\bar{C_{lw}}$', r'$\bar{C_{lw}}/\bar{C_{ls}}$', r'$\bar{C_{dw}}$',
+    r'$\bar{C_{dw}}/\bar{C_{ds}}$', r'$\bar{C_{lw}}/\bar{C_{dw}}$'
+]
+#---------------------------------------
+cd = os.getcwd()
+wd = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(cd))),
+                  'wake_capture_2d_AIAA_data')
+image_out_path = cd
+#---------------------------------------
+mcf_array_pa90 = []
+for cfi in cfd_data_list_pa90:
+    cfd_datai = os.path.join(wd, '1_SIM_RESULTS', cfi)
+    mcf_arrayi = read_cfd_data(cfd_datai)
+    mcf_array_pa90.append(mcf_arrayi)
+
+data_array_pa90 = mcf_array_pa90
+figname90 = 'pa90'
+#---------------------------------------
+
+cf_plotter90(x_data, data_array_pa90, marks90, legends, x_range, y_range,
+             y_label, image_out_path, figname90)
+# cf_plotter0(x_data, data_array_pa0, marks0, legends, x_range, y_range,
+# y_label, image_out_path, figname0)
---
+++ wake_convection_appendix/figure_transient_forces/transient_at/transientf_figure_at.py
@@ -0,0 +1,50 @@
+"""plotting cfd run results"""
+import os
+
+from tfplotting_functions_at import cf_plotter, read_cfd_data
+
+#-------------input plot control----------
+Re = [100, 1000]
+stroke = [3, 5, 7, 9]
+at = [0.125, 0.25, 0.5]
+pt = [0.25]
+#-----------------------------------------
+cfd_data_list = []
+for re in Re:
+    for s in stroke:
+        for a in at:
+            for p in pt:
+                cfd_data_name = 'Re' + '{0:.1f}'.format(
+                    re) + '_stroke' + '{0:.1f}'.format(
+                        s) + '_acf' + '{0:.3g}'.format(
+                            a) + '_pf' + '{0:.3g}'.format(p)
+                cfd_data_list.append(cfd_data_name)
+#-----------------------------------------
+markre = [r'$Re = 10^2$', r'$Re = 10^3$']
+marksc = [r'$s/c = ' + '{0:.1f}'.format(x) + '$' for x in stroke]
+markat = [r'$\^a_t = ' + '{0:.3g}'.format(x) + '$' for x in at]
+marks = [markre, marksc, markat]
+#---------------------------------------
+time_to_plot = 'all'
+coeffs_show_range = 'all'
+time_to_plot = [0, 2.0]
+coeffs_show_range = [-3.0, 8]
+cycle_time = 1.1
+#---------------------------------------
+cd = os.getcwd()
+wd = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(cd)))),
+    'wake_capture_2d_AIAA_data/1_SIM_RESULTS/SIM_RESULTS_pa90')
+image_out_path = cd
+#---------------------------------------
+cf_array = []
+for cfi in cfd_data_list:
+    cfd_datai = os.path.join(wd, cfi)
+    cf_arrayi = read_cfd_data(cfd_datai)
+    cf_array.append(cf_arrayi)
+
+data_array = cf_array
+#---------------------------------------
+
+cf_plotter(data_array, marks, time_to_plot, coeffs_show_range, image_out_path,
+           cycle_time, 'against_t')
